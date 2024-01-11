1dfad6058da22ac390e4cb2e48818540005fb3c7
Jose A. Mendez Santiago <jamsmendez02@gmail.com>
2024-01-11 10:28:46 -0600

    maybe deprecate clipboard

--- init.lua
+++ init.lua
@@ -176,19 +176,19 @@ return {
     --   },
     -- }
 
-    vim.cmd [[
-      let g:clipboard = {
-      \   'name': 'win32yank-wsl',
-      \   'copy': {
-      \      '+': 'win32yank.exe -i --crlf',
-      \      '*': 'win32yank.exe -i --crlf',
-      \    },
-      \   'paste': {
-      \      '+': 'win32yank.exe -o --lf',
-      \      '*': 'win32yank.exe -o --lf',
-      \   },
-      \   'cache_enabled': 1,
-      \ }
-    ]]
+    -- vim.cmd [[
+    --   let g:clipboard = {
+    --   \   'name': 'win32yank-wsl',
+    --   \   'copy': {
+    --   \      '+': 'win32yank.exe -i --crlf',
+    --   \      '*': 'win32yank.exe -i --crlf',
+    --   \    },
+    --   \   'paste': {
+    --   \      '+': 'win32yank.exe -o --lf',
+    --   \      '*': 'win32yank.exe -o --lf',
+    --   \   },
+    --   \   'cache_enabled': 1,
+    --   \ }
+    -- ]]
   end,
 }
